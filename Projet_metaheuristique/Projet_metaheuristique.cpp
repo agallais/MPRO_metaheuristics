2602a771fe70855cc462cc3e0fe3347b5916ead2
PC-AYMERI\Aymeri <aymeri.gallais@polytechnique.edu>
2017-10-20 16:11:06 +0200

    Project begins in Visual Studio.

---
+++ Projet_metaheuristique/Projet_metaheuristique.cpp
@@ -0,0 +1,18 @@
+// Projet_metaheuristique.cpp : définit le point d'entrée pour l'application console.
+//
+
+#include "stdafx.h"
+#include <iostream>
+
+
+int main()
+{
+	std::cout << "Hello World ! " << std::endl;
+	int n;
+	std::cin >> n;
+
+
+
+    return 0;
+}
+
