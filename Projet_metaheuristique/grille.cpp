8663c2afef102a9b9ce0455f602c9a21d8f08463
PC-AYMERI\Aymeri <aymeri.gallais@polytechnique.edu>
2017-10-24 13:34:28 +0200

    Introduction of a probabilistic approach

--- Projet_metaheuristique/grille.cpp
+++ Projet_metaheuristique/grille.cpp
@@ -7,6 +7,7 @@
 
 using namespace std;
 
+
 int max(int a, int b) {
 
 	if (a > b) {
@@ -21,7 +22,7 @@ Grille::Grille(int n, int r_captation, int r_communication)
 	this->radius_of_captation = r_captation;
 	this->radius_of_communication = r_communication;
 	this->grid_size = n;
-	
+
 	this->non_covered_points = pow(n, 2) - 1;
 	this->map = new int*[this->grid_size];
 
@@ -80,7 +81,7 @@ void Grille::addSensor(int i, int j)
 		for (int l = max(j - this->radius_of_captation, 0); l <= j + this->radius_of_captation && l < this->grid_size; ++l) {
 
 
-			if ((((k - i) * (k - i)) + ((l - j) *(l-j))) <= (pow(this->radius_of_captation,2) )) {
+			if ((((k - i) * (k - i)) + ((l - j) *(l - j))) <= (pow(this->radius_of_captation, 2))) {
 
 				if (this->map[k][l] == 0) { neighbors.push_back(pair<int, int>(k, l)); }
 
@@ -90,14 +91,18 @@ void Grille::addSensor(int i, int j)
 
 
 	if (neighbors.empty()) { return; }
+
 	else
 	{
-		vector<pair<int, int>>::iterator neighborsIterator;
-		for (neighborsIterator = neighbors.begin(); neighborsIterator != neighbors.end(); neighborsIterator++) {
-			this->map[neighborsIterator->first][neighborsIterator->second] = 1;
+		double probability_of_being_selected = ((double)neighbors.size) / (this->nbDeVoisinsMax(i,j));
+		if (rand() < probability_of_being_selected) {
+			vector<pair<int, int>>::iterator neighborsIterator;
+			for (neighborsIterator = neighbors.begin(); neighborsIterator != neighbors.end(); neighborsIterator++) {
+				this->map[neighborsIterator->first][neighborsIterator->second] = 1;
 
+			}
+			this->map[i][j] = 3;// 3 indicates that a connected sensor is in this point of the grid
 		}
-		this->map[i][j] = 3;// 3 indicates that a connected sensor is in this point of the grid
 	}
 
 
@@ -164,6 +169,18 @@ void Grille::printGrid()
 	}
 	int ok;
 	cin >> ok;
+}
+
+double Grille::nbDeVoisinsMax(int i, int j) {
+	double nbDeVoisins = 0;
+	for (int k = max(0 ,i - this->radius_of_captation); k <= this->radius_of_captation +i && k <= this->grid_size; ++k) {
+		for (int l = max(0 ,j - this->radius_of_captation); l <= this->radius_of_captation + i && l <= this->grid_size; ++l) {
+			if (pow(k - i , 2) + pow(l - j , 2) <= pow(this->radius_of_captation, 2)) {
+				nbDeVoisins += 1.0;
+			}
+		}
+	}
+	return nbDeVoisins;
 }
 
 Grille::~Grille()
